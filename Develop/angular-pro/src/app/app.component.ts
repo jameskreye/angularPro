86b05d7b3dddbc0d00619650964ad9c2449c67fd
JamesKreye <jamesonmichel16@gmail.com>
2021-09-21 02:23:26 -0300

    passing  template variable to the view

--- Develop/angular-pro/src/app/app.component.ts
+++ Develop/angular-pro/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef, AfterContentInit, AfterViewInit, ChangeDetectorRef, ComponentRef } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild, ViewContainerRef, AfterContentInit, AfterViewInit, ChangeDetectorRef, ComponentRef, TemplateRef } from '@angular/core';
 import { AuthFormComponent } from './auth-form/auth-form.component';
 
 import { User } from './auth-form/auth-form.interface';
@@ -6,9 +6,7 @@ import { User } from './auth-form/auth-form.interface';
 @Component({
   selector: 'app-root',
   template: `
-  
-  <button (click)="destroyComponent()" style="margin-left: 15px;">Destroy</button>
-  <button (click)="moveComponent()" style="margin-left: 15px;">Move</button>
+
     <div>
       
       <!-- <auth-form 
@@ -20,47 +18,24 @@ import { User } from './auth-form/auth-form.interface';
       </auth-form> -->
       
       <div #entry></div>
+      <ng-template #tmpl>
+        Jameson Lamour: Santiago CL
+      </ng-template>
     </div>
   `
 })
 export class AppComponent implements AfterViewInit{
 
-  component: ComponentRef<AuthFormComponent>;
 
   //to communicate to that piece here in the DOM
   @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef
 
-  constructor(
-    private resolver: ComponentFactoryResolver,
-    private cd: ChangeDetectorRef
-  ){}
-  
+  @ViewChild('tmpl') tmpl: TemplateRef<any>;
+
 
   ngAfterViewInit() {
-    const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-
-    this.entry.createComponent(authFormFactory); // this one now has index -1 by default
-    // create the actual component and place it in entry placeholder.
-    this.component = this.entry.createComponent(authFormFactory, 0); // can pass the index to order it just like javascript array
-    this.component.instance.title = 'Create account'; //overriding the actual value of title, so it can be dynamic
-    this.component.instance.submitted.subscribe(this.loginUser) // subscribing to the output from the AuthFormComponent class.
     
-
-    this.cd.detectChanges();
+    this.entry.createEmbeddedView(this.tmpl);
   }
-
-  loginUser(user: User) {
-    console.log('Login', user);
-  }
-
-  destroyComponent() {
-    this.component.destroy();
-  }
-
-  moveComponent() {
-    this.entry.move(this.component.hostView, 1)
-  }
-
-
 
 }
