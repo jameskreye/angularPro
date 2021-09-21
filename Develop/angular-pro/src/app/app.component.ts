0f4c4adb3a21e747106001d6cb03277835c0798c
JamesKreye <jamesonmichel16@gmail.com>
2021-09-21 02:41:32 -0300

    usuing ngTemplateOutlet

--- Develop/angular-pro/src/app/app.component.ts
+++ Develop/angular-pro/src/app/app.component.ts
@@ -17,38 +17,37 @@ import { User } from './auth-form/auth-form.interface';
         </button>
       </auth-form> -->
       
-      <div #entry></div>
-      <ng-template #tmpl let-name let-location="location">
-        {{name}} : {{location}}
+      <ng-container 
+      [ngTemplateOutlet]="tmpl"></ng-container>
+      <ng-template #tmpl >
+      Jameson Lamour : Santiago CL
       </ng-template>
     </div>
   `
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent{
 
 
   //to communicate to that piece here in the DOM
-  @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef
+  // @ViewChild('entry', {read: ViewContainerRef}) entry: ViewContainerRef
 
-  @ViewChild('tmpl') tmpl: TemplateRef<any>;
+  // @ViewChild('tmpl') tmpl: TemplateRef<any>;
 
-  constructor(
-    private resolver: ComponentFactoryResolver,
-    private cd: ChangeDetectorRef
-  ){}
+  // constructor(
+  //   private resolver: ComponentFactoryResolver,
+  //   private cd: ChangeDetectorRef
+  // ){}
 
-  ngAfterViewInit() {
+  // ngAfterViewInit() {
     
-      // this behaves like the *ngFor of angular, thats how it passes the index, odd etc...
-    this.entry.createEmbeddedView(this.tmpl, {
-      //passing the context
-      $implicit: ' Jameson Lamour', // the implicit can be referenced by any name in the template
-      location: 'Santiago CL'
-    });
+  //     // this behaves like the *ngFor of angular, thats how it passes the index, odd etc...
+  //   this.entry.createEmbeddedView(this.tmpl, {
+  //     //passing the context
+  //     $implicit: ' Jameson Lamour', // the implicit can be referenced by any name in the template
+  //     location: 'Santiago CL'
+  //   });
 
-    this.cd.detectChanges();
-  }
+  //   this.cd.detectChanges();
 
-  
 
 }
